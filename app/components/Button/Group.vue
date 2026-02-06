b52eec5c99aaa284e2442672b154d5b4fe6c2fb9
Marcus Blättermann <marcus@essenmitsosse.de>
2026-02-06 05:30:38 +0100

    fix: l-to-r

--- app/components/Button/Group.vue
+++ app/components/Button/Group.vue
@@ -1,6 +1,6 @@
 <template>
   <nav
-    class="flex items-center shrink-0 ms-auto [&>*:not(:first-child)]:rounded-l-none [&>*:not(:first-child)]:border-l-0 [&>*:not(:last-child)]:rounded-r-none"
+    class="flex items-center shrink-0 ms-auto [&>*:not(:first-child)]:rounded-s-none [&>*:not(:first-child)]:border-s-0 [&>*:not(:last-child)]:rounded-e-none"
   >
     <slot />
   </nav>
